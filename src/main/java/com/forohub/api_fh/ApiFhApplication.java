dc0bbba5aca6c16456cf7360e9aaab973cf6891a
DiegoFranco16 <diego.alejandrof@hotmail.com>
2025-01-10 18:58:52 -0500

    Primer commit

---
+++ src/main/java/com/forohub/api_fh/ApiFhApplication.java
@@ -0,0 +1,13 @@
+package com.forohub.api_fh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forohub/api_fh/ApiFhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.api_fh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
